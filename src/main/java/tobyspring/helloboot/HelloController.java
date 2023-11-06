b3cd00a5d5b4aef57957d684c2052c718d0423b7
gon-song <gon.song@balancehero.com>
2023-11-06 10:22:34 +0900

    디스패쳐 서블릿

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -2,15 +2,22 @@ package tobyspring.helloboot;
 
 import java.util.Objects;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
     public HelloController(SimpleHelloService helloService) {this.helloService = helloService;}
 
-    @GetMapping("/hello")
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
+
